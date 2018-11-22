ee2e2983b3786bafb052635f67466501e50d9a51
ballweera <ballweera.kasetsin@gmail.com>
2018-11-22 23:15:01 +0700

    Can produce message to example topic succcessfully

--- sarama_producer.go
+++ sarama_producer.go
@@ -1,12 +1,37 @@
 package kafkakit
 
+import (
+	"log"
+
+	"github.com/Shopify/sarama"
+)
+
 // SaramaProducer implements kafkakit/Producer with Shopify/sarama
 type SaramaProducer struct {
 }
 
 // Produce sends message to Kafka topic
 func (smp *SaramaProducer) Produce(msg string) (ok bool) {
-	return false
+	producer, err := sarama.NewAsyncProducer([]string{"localhost:9092"}, nil)
+
+	if err != nil {
+		log.Fatal(err)
+		return false
+	}
+
+	defer func() {
+		if err := producer.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	producer.Input() <- &sarama.ProducerMessage{
+		Topic: "example",
+		Key:   nil,
+		Value: sarama.StringEncoder(msg),
+	}
+
+	return true
 }
 
 // NewSaramaProducer creates SaramaProducer pointer
